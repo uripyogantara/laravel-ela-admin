79d0e3fd700356424ee770fb7db8ac375f127482
Urip Yogantara <uripyogantara@gmail.com>
2018-04-07 13:10:46 +0800

    dashboard template

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,46 @@
+<div class="header">
+        <nav class="navbar top-navbar navbar-expand-md navbar-light">
+            <!-- Logo -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.html">
+                    <!-- Logo icon -->
+                <b><img src="{{asset('images/logo-ti.png')}}" width="30px" alt="homepage" class="dark-logo" /></b>
+                    <!--End Logo icon -->
+                    <!-- Logo text -->
+                    <span><img src="{{asset('images/logo-text.png')}}" alt="homepage" class="dark-logo" /></span>
+                </a>
+            </div>
+            <!-- End Logo -->
+            <div class="navbar-collapse">
+                <!-- toggle and nav items -->
+                <ul class="navbar-nav mr-auto mt-md-0">
+                    <!-- This is  -->
+                    <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted  " href="javascript:void(0)"><i class="mdi mdi-menu"></i></a> </li>
+                    <li class="nav-item m-l-10"> <a class="nav-link sidebartoggler hidden-sm-down text-muted  " href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
+                </ul>
+                <!-- User profile and search -->
+                <ul class="navbar-nav my-lg-0">
+
+                    <!-- Search -->
+                    <li class="nav-item hidden-sm-down search-box"> <a class="nav-link hidden-sm-down text-muted  " href="javascript:void(0)"><i class="ti-search"></i></a>
+                        <form class="app-search">
+                            <input type="text" class="form-control" placeholder="Search here"> <a class="srh-btn"><i class="ti-close"></i></a> </form>
+                    </li>
+                    
+                    <!-- Profile -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="images/users/5.jpg" alt="user" class="profile-pic" /></a>
+                        <div class="dropdown-menu dropdown-menu-right animated zoomIn">
+                            <ul class="dropdown-user">
+                                <li><a href="#"><i class="ti-user"></i> Profile</a></li>
+                                <li><a href="#"><i class="ti-wallet"></i> Balance</a></li>
+                                <li><a href="#"><i class="ti-email"></i> Inbox</a></li>
+                                <li><a href="#"><i class="ti-settings"></i> Setting</a></li>
+                                <li><a href="#"><i class="fa fa-power-off"></i> Logout</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,49 @@
+<div class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li class="nav-devider"></li>
+                
+                <li class="nav-label">Home</li>
+                <li> <a href="kontol" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Dashboard</span></a>
+                </li>
+
+                <li class="nav-label">Pasien</li>
+                <li> 
+                    <a href="#" aria-expanded="false">
+                        <i class="fa fa-stethoscope"></i>
+                        <span class="hide-menu">Kunjungan</span>
+                    </a>
+                </li>
+
+                <li class="nav-label">Data</li>
+                <li> 
+                    <a href="#" aria-expanded="false">
+                        <i class="fa fa-database"></i>
+                        <span class="hide-menu">Data Master</span>
+                    </a>
+                </li>
+                <li> 
+                    <a href="#" aria-expanded="false">
+                        <i class="fa fa-user-md"></i>
+                        <span class="hide-menu">Data Pasien</span>
+                    </a>
+                </li>
+
+                <li class="nav-label">Akun</li>
+                <li> 
+                    <a href="#" aria-expanded="false">
+                        <i class="fa fa-gear"></i>
+                        <span class="hide-menu">Pengaturan </span>
+                    </a>
+                </li>
+
+                
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</div>
